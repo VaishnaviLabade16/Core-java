53e2a2528051eb768356dd859a96ee893ecbcb9b
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2025-09-15 23:26:34 +0530

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,21 @@
+
+public class Bird {
+
+	public int age;
+	public String name;
+	public String color;
+	
+	Bird()
+	{
+		this.age=20;
+		this.name="paushya";
+		this.color="black";
+	}
+	
+	Bird(String name,String color,int age)
+	{
+		this.name=name;
+		this.color=color;
+		this.age=age;
+	}
+}
