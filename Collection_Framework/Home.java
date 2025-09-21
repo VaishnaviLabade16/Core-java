aeb0e0a05090a60d0686f9c5a79e99b32e39a849
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2025-09-21 22:57:55 +0530

    Add files via upload

---
+++ Collection_Framework/Home.java
@@ -0,0 +1,14 @@
+package Collection_Framework;
+import java.util.*;
+public class Home 
+{
+  public static void main(String args[])
+  {
+	 // HashSet<object> hs=new HashSet<object>();
+	 // Set.add(21);
+	  //set.add(122);
+	// System.out.println(Set);
+     //HashSet<object> hs=new Hashset<object>();
+     
+  }
+}
