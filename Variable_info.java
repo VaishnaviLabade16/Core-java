751a7e9c6493cc1486a88295c57b19f5944f9040
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2025-09-28 14:36:59 +0530

    Add files via upload

---
+++ Variable_info.java
@@ -0,0 +1,14 @@
+
+public class Variable_info 
+{
+   public static void main(String args[])
+   {
+	Variable v = new Variable();
+	
+	
+	v.setName("vaishnavi");
+	
+	System.out.println(v.getName());
+	
+    }
+}
